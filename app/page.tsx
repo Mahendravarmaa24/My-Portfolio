3689403ffe6ba887d24c39045399202b3d5172ec
Mahendravarmaa Anbarasan <varmaa.mahi@gmail.com>
2026-03-22 02:11:49 -0500

    fixed social links finally

--- app/page.tsx
+++ app/page.tsx
@@ -144,7 +144,7 @@ export default function MahendravarmaaPortfolio() {
                   Download Resume
                 </a>
                 <a
-                  href="www.linkedin.com/in/mahendravarmaa-anbarasan-53248a282"
+                  href="https://www.linkedin.com/in/mahendravarmaa-anbarasan-53248a282/"
                   target="_blank"
                   rel="noreferrer"
                   className="rounded-2xl border border-red-900 px-5 py-3 font-semibold text-zinc-100 transition hover:border-red-700 hover:bg-red-950/30"
@@ -378,9 +378,13 @@ export default function MahendravarmaaPortfolio() {
             >
               Email Me
             </a>
-            <a href="https://github.com/Mahendravarmaa24">
+            <a 
+              href="https://github.com/Mahendravarmaa24"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="rounded-2xl border border-red-900 px-5 py-3 font-semibold text-zinc-100 transition hover:border-red-700 hover:bg-red-950/30"
 
-            
+            >
               GitHub
             </a>
 
